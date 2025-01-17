b8e6430cdaa2d48c8665e969d22e3fcc14163f62
nevse <nov.eugen@gmail.com>
2025-01-17 17:43:39 +0400

    net9

--- CS/App.xaml.cs
+++ CS/App.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace maui_gauges {
+﻿
+namespace maui_gauges {
     public partial class App : Application {
         public App() {
             InitializeComponent();
+        }
 
-            MainPage = new AppShell();
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            return new Window(new AppShell());
         }
     }
 }
